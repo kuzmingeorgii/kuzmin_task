b00426612aa5951037485def4b4a94ce4c3e16de
kuzmingeorgii <kuzmin098765@gmail.com>
2024-12-19 23:32:14 +0300

    third version

--- venv/main.py
+++ venv/main.py
@@ -1,58 +1,8 @@
-import io
 import sys
 import random
-from PyQt6 import uic
 from PyQt6.QtWidgets import QPushButton, QApplication, QMainWindow
 from PyQt6.QtGui import QPainter, QColor
 from PyQt6.QtCore import QRect
-
-
-template = """<?xml version="1.0" encoding="UTF-8"?>
-<ui version="4.0">
- <class>MainWindow</class>
- <widget class="QMainWindow" name="MainWindow">
-  <property name="geometry">
-   <rect>
-    <x>0</x>
-    <y>0</y>
-    <width>800</width>
-    <height>600</height>
-   </rect>
-  </property>
-  <property name="windowTitle">
-   <string>MainWindow</string>
-  </property>
-  <widget class="QWidget" name="centralwidget">
-   <widget class="QPushButton" name="pushButton">
-    <property name="geometry">
-     <rect>
-      <x>10</x>
-      <y>0</y>
-      <width>113</width>
-      <height>32</height>
-     </rect>
-    </property>
-    <property name="text">
-     <string>Нажми меня</string>
-    </property>
-   </widget>
-  </widget>
-  <widget class="QMenuBar" name="menubar">
-   <property name="geometry">
-    <rect>
-     <x>0</x>
-     <y>0</y>
-     <width>800</width>
-     <height>38</height>
-    </rect>
-   </property>
-  </widget>
-  <widget class="QStatusBar" name="statusbar"/>
- </widget>
- <resources/>
- <connections/>
-</ui>
-"""
 
 
 class MainWindow(QMainWindow):
@@ -65,22 +15,33 @@ class MainWindow(QMainWindow):
     def initUI(self):
         self.pushButton.clicked.connect(self.add_circle)
 
-        self.circles = list()
+        self.setGeometry(100, 100, 800, 600)
+        self.setWindowTitle("Случайные окружности")
+
+        self.pushButton = QPushButton("Нажми меня", self)
+        self.pushButton.setGeometry(10, 10, 113, 32)
+        self.pushButton.clicked.connect(self.add_circle)
+
+        self.circles = []
 
     def add_circle(self):
         diameter = random.randint(10, 100)
+
         x = random.randint(0, self.width() - diameter)
-        y = random.randint(0, self.height() - diameter - 50)
-        self.circles.append((x, y, diameter))
+        y = random.randint(50, self.height() - diameter)
+
+        color = QColor(random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
+
+        self.circles.append((x, y, diameter, color))
 
         self.update()
 
     def paintEvent(self, event):
         painter = QPainter(self)
-        painter.setBrush(QColor(255, 255, 0))
-        painter.setPen(QColor(0, 0, 0))
 
-        for x, y, diameter in self.circles:
+        for x, y, diameter, color in self.circles:
+            painter.setBrush(color)
+            painter.setPen(QColor(0, 0, 0))
             painter.drawEllipse(QRect(x, y, diameter, diameter))
 
 
